4b8c3d79e7b3c802ca98f330a64114cee424df30
Robson Alessio <69646857+RobsonAlessio@users.noreply.github.com>
2021-12-07 15:21:52 -0300

    Alterações do Firebase e do ESP8266

--- arduino/sensoresESP8266.ino
+++ arduino/sensoresESP8266.ino
@@ -1,75 +1,116 @@
 
-
+//Importações das bibliotecas
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 #include <Ticker.h>
 
-// Set these to run example.
-#define FIREBASE_HOST "sua_url_do_firebase_aqui.firebaseio.com"
-#define FIREBASE_AUTH "segredo_do_firebase_aqui"
+//Definindo o database do Firebase
+#define FIREBASE_HOST "abp-iot-2-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "XmMpRoPvM0LHhQVEqx0DWGmneob13BuxeHiGOsrO"
+//Definindo a rede WiFi
 #define WIFI_SSID "wifi_da_sua_casa"
 #define WIFI_PASSWORD "senha_da_wifi"
 
-#define DHT_PIN D5
-// Publique a cada 5 min
-#define PUBLISH_INTERVAL 2000
-
+//Definição das variáveis
+#define LED_ONBOARD LED_BUILTIN
+const int echoPin = D2; 
+const int trigPin = D3;
+float distancia; 
+String distanciaRob;
+const int IN_A0 = A0;
+int luminosidadeRob;
+// Publicação a cada 2 segundos
+#define PUBLISH_INTERVAL 2000;
 Ticker ticker;
 bool publishNewState = true;
 
-void publish(){
-  publishNewState = true;
-}
+//Declaração das variáveis
+void publish();
+void setupWifi();
+void setupFirebase();
+void setupPinos();
+void hcsr04();
+void sensorLuminosity();
 
-
-void setupWifi(){
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
-}
-
-void setupFirebase(){
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.setBool("lamp", false);
-  Firebase.setBool("presence", false);
-}
-
+//VOID SETUP
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   setupWifi();    
-
   setupFirebase();
-
+  setupPinos()
   // Registra o ticker para publicar de tempos em tempos
   ticker.attach_ms(PUBLISH_INTERVAL, publish);
 }
 
+//VOID LOOP
 void loop() {
 
   // Apenas publique quando passar o tempo determinado
   if(publishNewState){
     Serial.println("Publicando novo estado: ");
-    // Obtem os dados do sensor DHT 
-    float humidity = dht.readHumidity();
-    float temperature = dht.readTemperature();
+    // Obtem os dados dos sensores
+    hcsr04();
+    sensorLuminosity();
 
-    if(!isnan(humidity) && !isnan(temperature)){
-      // Manda para o firebase
-      Firebase.pushFloat("temperature", temperature);
-      Firebase.pushFloat("humidity", humidity);    
+    if(!isnan(distanciaRob) && !isnan(luminosidadeRob)){
+      // Mandando para o Firebase
+      Serial.println("Distância Rob: " +(String)distanciaRob);
+      Serial.println("Luminosidade Rob: " +(String)luminosidadeRob);
+      Firebase.pushFloat("distanciaRob", distanciaRob);
+      Firebase.pushFloat("luminosidadeRob", luminosidadeRob);    
       publishNewState = false;
     }else{
       Serial.println("Erro na publicação...");
     }
   }
-
-  
   delay(200);
+}
+
+
+//Funções criadas
+
+void publish(){
+  publishNewState = true;
+}
+
+void setupWifi(){
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Conectando");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Conectado - IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setupFirebase(){
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+void setupPinos(){
+  pinMode(echoPin, INPUT); 
+  pinMode(trigPin, OUTPUT); 
+  pinMode (IN_A0, INPUT);
+  pinMode(LED_ONBOARD, OUTPUT);
+}
+
+void hcsr04(){
+    digitalWrite(trigPin, LOW); 
+    delayMicroseconds(2); 
+    digitalWrite(trigPin, HIGH); 
+    delayMicroseconds(10); 
+    digitalWrite(trigPin, LOW); 
+    unsigned long int tempo = pulseIn(echoPin,HIGH); //Retorna o tempo em microsegundos 
+    //Distancia = Velocidade(Cm/Ms)*(tempo/2)
+    distancia = ((0.034*(tempo/2))/100);
+    distanciaRob = String(distancia);
+    delay(200);
+}
+
+void sensorLuminosity(){
+  luminosidadeRob = analogRead(IN_A0);
 }
--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 
 (function(){
     
-  // Inicia o firebase Firebase
+  // Inicia o Firebase
   var config = {
     apiKey: "XmMpRoPvM0LHhQVEqx0DWGmneob13BuxeHiGOsrO",
     authDomain: "abp-iot-2.com",
@@ -14,73 +14,24 @@
   var db = firebase.database();
 
   // Cria os listeners dos dados no firebase
-  var tempRef = db.ref('temperature');
-  var umidRef = db.ref('humidity');
-  var presenceRef = db.ref('presence');
-
+  var distRob = db.ref('distanciaRob');
+  var lumiRob = db.ref('luminosidadeRob');
+  var distMauricio = db.ref('distanciaMauricio');
+  var lumiMauricio = db.ref('luminosidadeMauricio');
 
 
   // Registra as funções que atualizam os gráficos e dados atuais da telemetria
-  tempRef.on('value',function (snapshot) {
-    document.getElementById("currentTemp").innerText = snapshot.val() + 'm';
+  distRob.on('value',function (snapshot) {
+    document.getElementById("distanciaRob").innerText = snapshot.val() + ' m';
   });
-  umidRef.on('value',function (snapshot) {
-    document.getElementById("currentUmid").innerText = snapshot.val();
+  lumiRob.on('value',function (snapshot) {
+    document.getElementById("luminosidadeRob").innerText = snapshot.val();
   });
-
-
-
-  // Registrar função ao alterar valor de presença
-  presenceRef.on('value', function(snapshot){
-    var value = snapshot.val();
-    var el = document.getElementById('currentPresence')
-    if(value){
-      el.classList.add('green-text');
-    }else{
-      el.classList.remove('green-text');
-    }
+  distMauricio.on('value',function (snapshot) {
+    document.getElementById("distanciaMauricio").innerText = snapshot.val() + ' m';
+  });
+  lumiMauricio.on('value',function (snapshot) {
+    document.getElementById("luminosidadeMauricio").innerText = snapshot.val();
   });
 
 })();
-
-
-// Retorna uma função que de acordo com as mudanças dos dados
-// Atualiza o valor atual do elemento, com a metrica passada (currentValueEl e metric)
-// e monta o gráfico com os dados e descrição do tipo de dados (chartEl, label)
-function onNewData(currentValueEl, chartEl, label, metric){
-  return function(snapshot){
-    var readings = snapshot.val();
-    if(readings){
-        var currentValue;
-        var data = [];
-        for(var key in readings){
-          currentValue = readings[key]
-          data.push(currentValue);
-        }
-        document.getElementById(currentValueEl).innerText = currentValue + ' ' + metric;
-        buildLineChart(chartEl, label, data);
-    }
-  }
-}
-
-// Constroi um gráfico de linha no elemento (el) com a descrição (label) e os
-// dados passados (data)
-function buildLineChart(el, label, data){
-  var elNode = document.getElementById(el);
-  new Chart(elNode, {
-    type: 'line',
-    data: {
-        labels: new Array(data.length).fill(""),
-        datasets: [{
-            label: label,
-            data: data,
-            borderWidth: 1,
-            fill: false,
-            spanGaps: false,
-            lineTension: 0.1,
-            backgroundColor: "#F9A825",
-            borderColor: "#F9A825"
-        }]
-    }
-  });
-}
